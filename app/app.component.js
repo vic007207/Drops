5b6d2f72f93c0110b5b07544f99315df2ca83056
vic007207 <vic3df@virginia.edu>
2016-10-03 16:50:16 -0400

    updated ap module and component

--- app/app.component.js
+++ app/app.component.js
@@ -1,10 +1,31 @@
+function Hero (id,name) {
+    this.id = id;
+    this.name = name;
+}
+
 (function(app) {
+
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular App</h1>'
+      template: `
+		  <h1>{{title}}</h1>
+		  <h2>{{hero.name}} details!</h2>
+		  <div><label>id: </label>{{hero.id}}</div>
+		  <div>
+		    <label>name: </label>
+		    <input [(ngModel)]="hero.name" placeholder="name">
+		  </div>
+  		`
     })
     .Class({
-      constructor: function() {}
+      constructor: function() {
+      	this.title = 'Tour of Heroes';
+  		this.hero = new Hero(1,"Windstorm");
+      }
     });
 })(window.app || (window.app = {}));
+
+
+
+
